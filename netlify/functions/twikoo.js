fff4f71b8fbd2a5640a6be5ed84194a5fc4e5431
lizilizzhh <lizzzhh@outlook.com>
2026-08-04 12:42:47 +0800

    fix

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,5 +1,23 @@
-const twikoo = require('twikoo-vercel')
-
+const twikoo = require("twikoo-vercel");
+const Ip2Region = require("ts-ip2region2");
+require("twikoo-func/utils/lib").getIpToRegion = () => {
+  return {
+    create() {
+      const searcher = new Ip2Region.Ip2Region();
+      return {
+        btreeSearchSync(ipStr) {
+          const result = searcher.search('ipStr');
+          let parts = result.region.split("|");
+          if (parts.length === 4) {
+            parts.splice(1, 0, "0"); // 在第二位置插入区域占位符
+          }
+          const fullRegion = parts.join("|");
+          return { region: fullRegion }
+        },
+      };
+    },
+  };
+};
 /**
  * Netlify 函数兼容 Vercel 函数实现
  * 复用 Twikoo Vercel 函数代码
@@ -7,41 +25,42 @@ const twikoo = require('twikoo-vercel')
  * https://docs.netlify.com/functions/create/?fn-language=js
  */
 exports.handler = async function (event, context) {
-  process.env.VERCEL_URL = event.rawUrl.replace(/^https?:\/\//, '')
+  process.env.VERCEL_URL = event.rawUrl.replace(/^https?:\/\//, "");
   process.env.TWIKOO_IP_HEADERS = JSON.stringify([
-    'headers.cf-connecting-ip','headers.x-nf-client-connection-ip'
-  ])
+    "headers.cf-connecting-ip",
+    "headers.x-nf-client-connection-ip",
+  ]);
   const result = {
     statusCode: 204,
     headers: {},
-    body: ''
-  }
+    body: "",
+  };
   const request = {
     method: event.httpMethod,
     headers: event.headers,
-    body: {}
-  }
+    body: {},
+  };
   try {
-    request.body = JSON.parse(event.body)
+    request.body = JSON.parse(event.body);
   } catch (e) {}
   const response = {
     status: function (code) {
-      result.statusCode = code
-      return this
+      result.statusCode = code;
+      return this;
     },
     json: function (json) {
-      result.headers['Content-Type'] = 'application/json'
-      result.body = JSON.stringify(json)
-      return this
+      result.headers["Content-Type"] = "application/json";
+      result.body = JSON.stringify(json);
+      return this;
     },
     end: function () {
-      return this
+      return this;
     },
     setHeader: function (k, v) {
-      result.headers[k] = v
-      return this
-    }
-  }
-  await twikoo(request, response)
-  return result
-}
+      result.headers[k] = v;
+      return this;
+    },
+  };
+  await twikoo(request, response);
+  return result;
+};
